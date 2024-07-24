1f39bf7354260a7787dd4b24a49d17972b024baa
Vladimir Fedorov <pew-pew-pew-2@mail.ru>
2024-07-24 03:38:14 +0300

    filters1

--- src/Controller/TestCategoryController.php
+++ src/Controller/TestCategoryController.php
@@ -10,9 +10,12 @@ use App\Entity\CatalogProduct;
 use App\Entity\CatalogCategory;
 use App\Repository\CatalogCategoryRepository;
 use App\Repository\CatalogProductRepository;
+use Elastica\Query\MatchQuery;
 use Elastica\Query\MultiMatch;
 use Elastica\Query\BoolQuery;
+use Elastica\Query\Range;
 use Elastica\Query\Term;
+use Elastica\Query\Nested;
 
 
 use JoliCode\Elastically\Factory;
@@ -29,7 +32,7 @@ class TestCategoryController extends AbstractController
     {
     }
 
-    #[Route('/test/category', name: 'app_test_trees')]
+    #[Route('/test/trees', name: 'app_test_trees')]
     public function index(CatalogCategoryRepository $repository): Response
     {
         $trees = $repository->getTrees();
@@ -45,76 +48,60 @@ class TestCategoryController extends AbstractController
         ]);
     }
 
-    #[Route('/test/category/{id}', name: 'app_test_print_tree')]
-    public function tree(
-        CatalogCategory           $root,
-        CatalogCategoryRepository $categoryRepository,
-        CatalogProductRepository  $productRepository,
-        Client                    $client,
-
-    ): Response
+    #[Route('/test/tree/{id}', name: 'app_test_print_tree')]
+    public function tree(CatalogCategory $root, CatalogCategoryRepository $categoryRepository, CatalogProductRepository $productRepository): Response
     {
         $children = $categoryRepository->getChildren($root, false, 'left', 'ASC', true);
 
-        /** @var @var CatalogProduct $product */
-        //$products = $children[0]->getProducts();
-        //foreach ($products as $product) {
-        //    $model = $this->productModelBuilder->create($product);
-        //    dump($model);
-        //    die();
-        //}
+        return $this->render('test_category/printTree.html.twig', [
+            'root' => $root,
+            'children' => $children,
+            'products' => []
+        ]);
+    }
 
-        //$ids = $categoryRepository->getAllChildrenIds($root, true);
-        //dump($children);
-        //die();
-        //dump($ids);
+    #[Route('/test/category/{id}', name: 'app_test_print_category')]
+    public function category(CatalogCategory $category, CatalogCategoryRepository $categoryRepository, Client $client): Response
+    {
+        dump($category);
 
-        //$results = $client->getIndex('product')->getAliases();
-        //dump($results);
-        //die();
+        $boolQuery = (new BoolQuery())
+            ->addFilter(new MatchQuery('categories.root', $category->getRoot()->getId())) // дерево этой категории
+            ->addFilter(new Range('categories.left', ['gte' => $category->getLeft()])) // все вложенные подкатегории
+            ->addFilter(new Range('categories.right', ['lte' => $category->getRight()])) // все вложенные подкатегории
+        ;
+
+        $nestedQuery = (new Nested())
+            ->setPath('categories')
+            ->setQuery($boolQuery)
+        ;
+
+        $query = new Query();
+        $query->setSize(50);
+        $query->setFrom(0);
+        $query->addSort(['updatedAt' => ['order' => 'DESC']]);
+        $query->setQuery($nestedQuery);
+
+        // todo: фильтр покажет сколько товаров в категории
+        // todo: пост фильтр покажет сколько товаров в каждой подкатегории
+
+        //$query->addAggregation()
+        //$query->setPostFilter()
+        //dump($boolQuery->toArray());
+        dump($nestedQuery->toArray());
+        dump($query->toArray());
+
+        $found = $client->getIndex('product')->search($query);
+        $products = array_map(fn($r) => $r->getModel(), $found->getResults());
+
 
         // https://elastica.io/getting-started/search-documents.html
         // https://hotexamples.com/examples/elastica.query/Bool/-/php-bool-class-examples.html
 
         // https://www.youtube.com/watch?v=2KgJ6TQPIIA&list=PL_mJOmq4zsHZYAyK606y7wjQtC0aoE6Es&index=3&ab_channel=OfficialElasticCommunity
         // https://www.youtube.com/watch?v=iGKOdep1Iss&list=PL_mJOmq4zsHZYAyK606y7wjQtC0aoE6Es&index=5
-        $searchText = 'printer';
 
-
-        $query = new Query();
-        $query->addSort(['updatedAt' => ['order' => 'DESC']]);
-
-        $boolQuery = new BoolQuery();
-        $boolQuery->addFilter(new Term(['enabled' => false]));
-
-        if (null !== $searchText) {
-            $searchQuery = new MultiMatch();
-            $searchQuery->setFields([
-                'name^5',
-                'name.autocomplete',
-                //'category.content',
-                //'category.authorName',
-            ]);
-            $searchQuery->setQuery($searchText);
-            $searchQuery->setType(MultiMatch::TYPE_MOST_FIELDS);
-            //$searchQuery->setParams(['size' => 1,'from'=> 2]);
-            //$searchQuery->addParam('sort', ['createdAt' => 'DESC']);
-
-            $boolQuery->addMust($searchQuery);
-        }
-
-        //$boolQuery-> addSort(['created_at' => $sortDirection]);
-        //$boolQuery->setParams(['size' => 1,'from'=> 2]);
-$query->setQuery($boolQuery);
-        $found = $client->getIndex('product')->search($query);
-        //$found = $client->getIndex('product')->search($boolQuery);
-        $results = [];
-        foreach ($found->getResults() as $result) {
-            dump($result->getModel());
-        }
-        die();
-
-
+        /*
         $query = 'C752n';
         dump($query);
         $searchQuery = new MultiMatch();
@@ -132,14 +119,12 @@ $query->setQuery($boolQuery);
         foreach ($found->getResults() as $result) {
             dump($result->getModel());
         }
-
         die();
+        */
 
-
-        return $this->render('test_category/printTree.html.twig', [
-            'root' => $root,
-            'children' => $children,
-            'products' => []
+        return $this->render('test_category/printCategory.html.twig', [
+            'category' => $category,
+            'products' => $products,
         ]);
     }
 }
